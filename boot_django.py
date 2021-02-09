b5f3e62970ac9065306c91cbbbd331dd46e7314d
Volkova-Natalia <volkova.natalia.1988@gmail.com>
2021-02-09 21:59:40 +0300

    (m) django-cors-headers is not required.

--- boot_django.py
+++ boot_django.py
@@ -24,13 +24,11 @@ def boot_django():
             'django.contrib.messages',
             'django.contrib.staticfiles',
             'rest_framework',
-            'corsheaders',
             "django_rest_auth_embedded",
         ],
         MIDDLEWARE=[
             'django.middleware.security.SecurityMiddleware',
             'django.contrib.sessions.middleware.SessionMiddleware',
-            'corsheaders.middleware.CorsMiddleware',
             'django.middleware.common.CommonMiddleware',
             'django.middleware.csrf.CsrfViewMiddleware',
             'django.contrib.auth.middleware.AuthenticationMiddleware',
